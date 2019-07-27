5d40aec028bfd52ce60e5c1cecbeac0e4ab834ab
Grzegorz Redlicki <redlicki.grzegorz@gmail.com>
2019-07-27 14:49:56 +0200

    tag function

---
+++ examples/functions/tag.py
@@ -0,0 +1,21 @@
+def tag(name, *content, cls=None, **attrs):
+    """Generate one or more HTML tags"""
+    if cls is not None:
+        attrs["class"] = cls
+    if attrs:
+        attr_str = "".join(f' {attr}="{value}"'
+                           for attr, value
+                           in sorted(attrs.items()))
+    else:
+        attr_str = ""
+    if content:
+
+        return "\n".join(f'<{name}{attr_str}>{c}</{name}>'
+                         for c in content)
+    else:
+        return f'<{name}{attr_str} />'
+
+if __name__ == '__main__':
+    t = {"wiek": 25, "cls" : 'Koalski'}
+
+    print(tag('Marian','Rudy', 'Wysoki', 'Biedny', 'Bezrobotny' , **t ))
