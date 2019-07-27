68d4bdc08839954d7881b4ca7ee0b7b72c45dad2
Grzegorz Redlicki <redlicki.grzegorz@gmail.com>
2019-07-27 11:30:38 +0200

    prepared new set of examples

--- examples/functions/default_arguments.py
+++ examples/functions/default_arguments.py
@@ -11,3 +11,10 @@ if __name__ == "__main__":
     print(format_phone_number('123456789'))
     print(format_phone_number('123456789', '+66'))
 
+    print(format_phone_number(area_code='+77', number='987654321'))
+    print(format_phone_number(area_code='+77', number='987654321', delimiter=':'))
+
+    print(format_phone_number('123123123', delimiter=' '))
+
+    print(format_phone_number('111111111', delimiter=':', area_code='+12'))
+
