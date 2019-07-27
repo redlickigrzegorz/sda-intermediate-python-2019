013b9c616988a8e72ab92ded943d7a66c67f8089
Grzegorz Redlicki <redlicki.grzegorz@gmail.com>
2019-07-27 11:54:52 +0200

    args and kwargs

---
+++ examples/functions/args_kwargs.py
@@ -0,0 +1,19 @@
+def sum_args(*args):
+    ret = 0
+    for arg in args:
+        ret += arg
+    return ret
+
+
+def sum_kwargs(**kwargs):
+    ret = 0
+    for key in kwargs:
+        ret += kwargs[key]
+    return ret
+
+
+
+
+if __name__ == '__main__':
+    print(sum_args(5, 8, 6, 3, 5))
+    print(sum_kwargs(a1=5, a2=8, a3=6, a4=3, a5=5))
