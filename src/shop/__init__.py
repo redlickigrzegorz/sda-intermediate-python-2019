da5b4269e98d81572e78760ac1785b61b995d733
Grzegorz Redlicki <redlicki.grzegorz@gmail.com>
2019-07-27 22:53:26 +0200

    Prepare the enigmatic function to show typing power

---
+++ src/shop/__init__.py
@@ -0,0 +1,17 @@
+from src.shop.customer import Customer
+from src.shop.order import Order
+
+
+def notify_everyone(orders):
+    for order in orders:
+        order.customer.notify()
+
+
+if __name__ == '__main__':
+    bob = Customer("Bob")
+    john = Customer("John")
+
+    order_1 = Order(bob)
+    order_2 = Order(john)
+
+    notify_everyone([order_1, order_2])
--- src/shop/customer.py
+++ src/shop/customer.py
@@ -1,3 +1,10 @@
-from collections import namedtuple
+import decimal
 
-Customer = namedtuple("Customer", ["name", "personal_discount"])
+
+class Customer:
+    def __init__(self, name, personal_discount=decimal.Decimal("0.00")):
+        self.name = name
+        self.personal_discount = personal_discount
+
+    def notify(self):
+        print(f"Customer {self.name} has been notified!")
