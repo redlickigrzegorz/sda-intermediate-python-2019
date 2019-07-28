f0da6cd80a5154492a0d3d50599f96fe6acaeea1
Grzegorz Redlicki <redlicki.grzegorz@gmail.com>
2019-07-28 09:48:41 +0200

    Swap trick

---
+++ examples/functions/swap_trick.py
@@ -0,0 +1,24 @@
+if __name__ == '__main__':
+    a = 1
+    b = 2
+    temp = b
+    b = a
+    a = temp
+    print(f'a= {a}')
+    print(f'b= {b}')
+
+    c = 4
+    d = 6
+    temp = (c, d)
+    print(temp)
+    d, c = temp
+    print(f'c= {c}')
+    print(f'd= {d}')
+
+    e = 8
+    f = 10
+    #temp2 = (e, f)
+    print(temp)
+    f, e = e, f
+    print(f'e= {e}')
+    print(f'f= {f}')
