1f98baed30e1a1921d49d0d3b9e8595ea822cd14
Grzegorz Redlicki <redlicki.grzegorz@gmail.com>
2019-07-28 15:37:45 +0200

    Mutable default arguments v2

--- examples/data_structures/mutable_argument.py
+++ examples/data_structures/mutable_argument.py
@@ -1,4 +1,5 @@
 import typing
+from datetime import datetime
 
 
 def add_dogs(name:str, dogs:typing.List[str] = []) -> typing.List[str]:
@@ -12,6 +13,15 @@ def add_dogs_properly(name:str, dogs:typing.List[str] = None) -> typing.List[str
     #     dogs = []
     dogs.append(name)
     return dogs
+
+
+def print_dt(dt: datetime = datetime.now()) -> None:
+    print(dt)
+
+
+def print_dt_properly(dt: datetime = None) -> None:
+    dt = dt or datetime.now()
+    print(dt)
 
 
 if __name__ == '__main__':
@@ -35,3 +45,17 @@ if __name__ == '__main__':
     print(b and a)
     print(a or b)
     print(b or a)
+
+    print("#######")
+    print_dt()
+    print_dt()
+    print_dt()
+    print_dt()
+    print_dt()
+
+    print("####### --> properly")
+    print_dt_properly()
+    print_dt_properly()
+    print_dt_properly()
+    print_dt_properly()
+    print_dt_properly()
