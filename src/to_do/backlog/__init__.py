3c2b26e47cdb4c5e9ad788d85713508431e5c07e
Grzegorz Redlicki <redlicki.grzegorz@gmail.com>
2019-08-03 14:32:58 +0200

    Backlog list of tasks v2

--- src/to_do/backlog/__init__.py
+++ src/to_do/backlog/__init__.py
@@ -1,12 +1,13 @@
 import typing
+from dataclasses import dataclass, field
 
 from src.to_do.backlog.task import BacklogTask
 
 
+@dataclass
 class Backlog:
-
-    def __init__(self) -> None:
-        self.tasks: typing.List[BacklogTask] = []
+    name: str = field(default='')
+    tasks: typing.List[BacklogTask] = field(default_factory=list)
 
     def add_task(self, task: BacklogTask) -> None:
         self.tasks.append(task)
